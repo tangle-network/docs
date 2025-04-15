c07c6b75ae4f4d009b71de82ac4344b658b2f849
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2025-04-16 04:13:12 +0700

    feat: setup static export for nextra

--- next.config.mjs
+++ next.config.mjs
@@ -17,41 +17,12 @@ const withBundleAnalyzer = bundleAnalyzer({
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   trailingSlash: false,
   staticPageGenerationTimeout: 120,
-  async redirects() {
-    return [
-      {
-        source: "/restake",
-        destination: "/restake/introduction/",
-        permanent: true,
-      },
-      {
-        source: "/developers",
-        destination: "/developers/blueprints/introduction",
-        permanent: true,
-      },
-      {
-        source: "/operators",
-        destination: "/operators/introduction/",
-        permanent: true,
-      },
-    ];
-  },
-  rewrites() {
-    return {
-      // beforeFiles: [
-      //   {
-      //     source: "/sitemap.xml",
-      //     destination:
-      //     //TODO: SUBMIT SITE MAP
-      //       "",
-      //   },
-      // ],
-    };
-  },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -63,6 +34,7 @@ const nextConfig = {
       },
     ],
   },
+  distDir: "dist",
 };
 
 export default withBundleAnalyzer(withNextra(nextConfig));
