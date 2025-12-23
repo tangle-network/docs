ad5514c2b559083996b3a3557729252cebaefc83
Drew Stone <drewstone329@gmail.com>
2025-12-22 23:54:47 -0700

    docs: add sandbox provisioning guide
    - add VM sandbox provisioning quick start
    - link provisioning from security and requirements
    - clarify CAP_NET_ADMIN target binary

--- pages/operators/manager/_meta.ts
+++ pages/operators/manager/_meta.ts
@@ -3,6 +3,7 @@ import { Meta } from "nextra";
 const meta: Meta = {
   introduction: "Introduction",
   requirements: "Requirements",
+  "sandbox-provisioning": "Sandbox Provisioning",
   setup: "Setup",
   sizing: "Sizing",
   security: "Sandboxing and Security",
