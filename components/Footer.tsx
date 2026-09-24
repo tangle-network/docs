e0806c67a889577a7c266de8379779539ae7edef
drewstone <drewstone329@gmail.com>
2026-09-24 01:06:09 -0700

    fix(docs): link footer to Tangle home (#186)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="w-full p-4 border-t border-gray-700 shadow bg-tangleLogoTextColor md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
         <span className="text-sm !text-gray-300 sm:text-center">
           © 2024{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="https://tangle.tools/" className="hover:underline">
             Tangle Foundation
           </a>
           . All Rights Reserved.
